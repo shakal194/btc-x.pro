44db9bc39ed1ff609ec05603cc7c14c9fabecdcc
Oleksandr Soloviov <shakal194@gmail.com>
2025-02-24 09:19:00 +0200

    fix livechat in mobile

--- src/components/ui/LiveChat.tsx
+++ src/components/ui/LiveChat.tsx
@@ -1,18 +1,45 @@
 'use client';
 
-
-import { LiveChatWidget, EventHandlerPayload } from '@livechat/widget-react'
+import { LiveChatWidget, EventHandlerPayload } from '@livechat/widget-react';
+import { useState, useEffect } from 'react';
 
 export default function LiveChat() {
+  const [visibility, setVisibility] = useState<'minimized' | 'maximized'>(
+    'maximized',
+  );
+
+  // Обработчик изменения размера окна
+  const handleResize = () => {
+    if (window.innerWidth < 1024) {
+      setVisibility('minimized');
+    } else {
+      setVisibility('maximized');
+    }
+  };
+
+  // Используем useEffect для добавления слушателя событий изменения размера окна
+  useEffect(() => {
+    // Устанавливаем начальную видимость
+    handleResize();
+
+    // Добавляем слушатель изменения размера
+    window.addEventListener('resize', handleResize);
+
+    // Убираем слушатель при размонтировании компонента
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   function handleNewEvent(event: EventHandlerPayload<'onNewEvent'>) {
-    console.log('LiveChatWidget.onNewEvent', event)
+    console.log('LiveChatWidget.onNewEvent', event);
   }
 
   return (
     <LiveChatWidget
-      license="18749046"
-      visibility="maximized"
+      license='18749046'
+      visibility={visibility} // Динамически меняем visibility
       onNewEvent={handleNewEvent}
     />
-  )
+  );
 }
