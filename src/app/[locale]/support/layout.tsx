71c378ddedc3a383aa2b89a99a752036be4c4663
Oleksandr Soloviov <shakal194@gmail.com>
2025-04-03 19:37:04 +0300

    добавление гугл аналитики

--- src/app/[locale]/support/layout.tsx
+++ src/app/[locale]/support/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import ButtonFooter from '@/components/ui/ButtonFooter';
 import FooterSection from '@/components/Footer';
 import LiveChat from '@/components/ui/LiveChat';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default async function Layout({
   children,
@@ -15,6 +16,7 @@ export default async function Layout({
       <ButtonFooter />
       <FooterSection />
       <LiveChat />
+      <GoogleAnalytics gaId='AW-16949187745' />
     </>
   );
 }
