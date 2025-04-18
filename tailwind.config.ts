b2e7e275b9cf3726eadffa4830639db9942bbd7e
Oleksandr Soloviov <shakal194@gmail.com>
2025-04-18 12:03:19 +0300

    фикс визуала

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ export default {
         'spin-slow': 'spin 5s linear infinite', // медленное вращение за 5 секунд
       },
       colors: {
+        'heroui-background': '#000000',
         background: '#000000',
         foreground: '#ffffff',
         danger: '#f31212',
